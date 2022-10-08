1cdb89a3b5014f1d722610f0190bf696e32afc57
Ayemere <davidodia4@gmail.com>
2022-10-09 00:31:45 +0100

    added negative unit test for saving new events

--- CountDownTimerTests/ViewModel/NewEventViewModelTest.swift
+++ CountDownTimerTests/ViewModel/NewEventViewModelTest.swift
@@ -10,8 +10,8 @@ import XCTest
 
 class NewEventViewModelTest: XCTestCase {
     
-    func test_saveNewEvent_DismissesView() {
-        let mockRepo = MockNewEventRepository()
+    func test_saveNewEvent_WhenSaveIs_Successfull_Calls_DismissesView() {
+        let mockRepo = MockNewEventRepository(saveStatus: true)
         let delegate = MockNewEventViewDelegate()
         let sut = MockNewEventViewModel(homeRepository: mockRepo)
         sut.delegate = delegate
@@ -25,6 +25,22 @@ class NewEventViewModelTest: XCTestCase {
         XCTAssertTrue(delegate.dismissAfterSaveViewDidCall)
         XCTAssertEqual(delegate.dismissAfterSaveViewCount, 1)
     }
+    
+    func test_saveNewEvent_WhenSaveIs_NotSuccessfull_Calls_DismissesView() {
+        let mockRepo = MockNewEventRepository(saveStatus: false)
+        let delegate = MockNewEventViewDelegate()
+        let sut = MockNewEventViewModel(homeRepository: mockRepo)
+        sut.delegate = delegate
+        sut.didChange(text: "Some Event name")
+        sut.didChange(date: Date.now)
+        
+        sut.saveNewEvent()
+        
+        XCTAssertTrue(mockRepo.createDidCall)
+        XCTAssertEqual(mockRepo.createCallCount, 1)
+        XCTAssertFalse(delegate.dismissAfterSaveViewDidCall)
+        XCTAssertEqual(delegate.dismissAfterSaveViewCount, 0)
+    }
 }
 
 
@@ -35,11 +51,16 @@ private class MockNewEventViewModel: NewEventViewModel {
 private class MockNewEventRepository: NewEventRepositoryProtocol {
     var createCallCount = 0
     var createDidCall = false
+    private let saveStatus: Bool
+    
+    init(saveStatus: Bool) {
+        self.saveStatus = saveStatus
+    }
     
     func create(futureEvent: FutureEvent, completionHandler: @escaping (Bool) -> ()) {
         createDidCall = true
         createCallCount += 1
-        completionHandler(true)
+        completionHandler(saveStatus)
     }
 }
 
